79d4cbd9fa22001edf4c04ae734ee61e4f0542b5
PandaKris <kristantosean@gmail.com>
2023-06-13 19:21:45 +0700

    Initial Commit

---
+++ ExplorersLog/ExplorersLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplorersLogApp.swift
+//  ExplorersLog
+//
+//  Created by Kristanto Sean on 13/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ExplorersLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
